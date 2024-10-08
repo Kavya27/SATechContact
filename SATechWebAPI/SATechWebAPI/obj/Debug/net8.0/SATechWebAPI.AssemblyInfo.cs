f35ecab6b6cd2e28ac8278b346494ce4dfa49100
Kavita Tuwar <kavita.tuwar27@gmail.com>
2024-10-08 16:55:17 +0530

    Create UI App

--- SATechWebAPI/SATechWebAPI/obj/Debug/net8.0/SATechWebAPI.AssemblyInfo.cs
+++ SATechWebAPI/SATechWebAPI/obj/Debug/net8.0/SATechWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SATechWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c986d24b0bb47106d8314d63dbca340bb50af8c2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SATechWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SATechWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
